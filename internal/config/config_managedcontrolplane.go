afb55b44a1653acd37ce1f2d449a5c734060b2dc
René Schünemann <rene.schuenemann@sap.com>
2025-10-23 11:17:17 +0200

    feat: allow to control the high availability config of ps mcp

--- internal/config/config_managedcontrolplane.go
+++ internal/config/config_managedcontrolplane.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/validation/field"
+	"k8s.io/utils/ptr"
 
 	commonapi "github.com/openmcp-project/openmcp-operator/api/common"
 	corev2alpha1 "github.com/openmcp-project/openmcp-operator/api/core/v2alpha1"
@@ -23,6 +25,21 @@ type ManagedControlPlaneConfig struct {
 	// A value of 0 disables the periodic reconciliation.
 	// +optional
 	ReconcileMCPEveryXDays int `json:"reconcileMCPEveryXDays,omitempty"`
+
+	// PlatformService specifies the configuration for the ManagedControlPlane platform service.
+	PlatformService ManagedControlPlaneConfigPlatformService `json:"platformService,omitempty"`
+}
+type ManagedControlPlaneConfigPlatformService struct {
+	// Replicas specifies the default number of replicas for ManagedControlPlane platform service.
+	// Default is 1.
+	// +optional
+	Replicas *int32 `json:"replicas,omitempty"`
+
+	// TopologySpreadConstraints specifies the default topology spread constraints for ManagedControlPlane platform service.
+	// More info: https://kubernetes.io/docs/concepts/workloads/pods/pod-topology-spread-constraints/
+	// The label selector for the topology spread constraints is set to match the labels of the ManagedControlPlane platform service.
+	// +optional
+	TopologySpreadConstraints []corev1.TopologySpreadConstraint `json:"topologySpreadConstraints,omitempty"`
 }
 
 func (c *ManagedControlPlaneConfig) Default(_ *field.Path) error {
@@ -34,6 +51,9 @@ func (c *ManagedControlPlaneConfig) Default(_ *field.Path) error {
 	}
 	if c.MCPClusterPurpose == "" {
 		c.MCPClusterPurpose = corev2alpha1.DefaultMCPClusterPurpose
+	}
+	if c.PlatformService.Replicas == nil {
+		c.PlatformService.Replicas = ptr.To[int32](1)
 	}
 	return nil
 }
@@ -55,6 +75,9 @@ func (c *ManagedControlPlaneConfig) Validate(fldPath *field.Path) error {
 		if c.DefaultOIDCProvider.Name == "system" {
 			errs = append(errs, field.Invalid(oidcFldPath.Child("name"), c.DefaultOIDCProvider.Name, "'system' is a reserved string and may not be used as name for the default OIDC provider"))
 		}
+	}
+	if c.PlatformService.Replicas != nil && *c.PlatformService.Replicas < 1 {
+		errs = append(errs, field.Invalid(fldPath.Child("replicas"), c.PlatformService.Replicas, "replicas must be at least 1"))
 	}
 
 	return errs.ToAggregate()
